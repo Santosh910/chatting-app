c641aeedb0e9a1279ded306b142e10a359f02d80
Santosh910 <138295487+Santosh910@users.noreply.github.com>
2024-03-14 03:33:14 +0530

    update

--- chatting-back/index.js
+++ chatting-back/index.js
@@ -29,10 +29,10 @@ app.get("/test",(req,res)=>{
 
 app.use("/api",router)
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/chat-front/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "chat-front", "dist", "index.html"));
 });
 
 
@@ -40,35 +40,3 @@ mongoose.connect(process.env.MONGOURI).then(()=>console.log("database connected"
 
 server.listen(PORT,()=>console.log(`app is running on port ${PORT}`))
 
-// const wss = new WebSocketServer({server});
-// wss.on('connection',(connection,req)=>{
-    
-//    const token = req.headers.token;
-//    if(!token){
-//       console.log('No token provided');
-//       connection.close();
-//       return;
-//    }
-
-//    try {
-//     const {id} = Jwt.verify(token,process.env.JWT_SEC)
-//     const user = UserModel.findById(id);
-//     if(!user){
-//         console.log('User not found');
-//         connection.close();
-//         return;
-//     }
-//     connection.send(JSON.stringify({user:{name:user.username,id:user._id},token}))
-//    } catch (error) {
-//         console.error('Token verification failed:',error.message)
-//    }
-
-// //    console.log(`Number of connected clients: ${wss.clients.size}`);
-
-// [...wss.clients].forEach(clients =>{
-//     clients.send(JSON.stringify(
-//         [...wss.clients].map(c => ({id:c.user._id,name:c.user.username}))
-//     ))
-// })
-// });
-// // console.log([...wss.clients].length)
